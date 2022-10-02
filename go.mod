55b6c4155884176acaa0a2cdf8cf1c01d82b8c1f
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-10-02 18:55:04 +0300

    New function added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/maxzhovtyj/go-invoice
 
 go 1.18
+
+require github.com/jung-kurt/gofpdf v1.16.2 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package go_invoice
+
+import (
+	"github.com/jung-kurt/gofpdf"
+	"log"
+)
+
+func New() {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddFont("Helvetica", "", "helvetica_1251.json")
+	pdf.AddPage()
+	pdf.SetFont("Helvetica", "", 16)
+	tr := pdf.UnicodeTranslatorFromDescriptor("cp1251")
+	pdf.Cell(15, 50, tr("русский текст"))
+	err := pdf.OutputFileAndClose("test.pdf")
+	if err != nil {
+		log.Println(err)
+	}
+}
