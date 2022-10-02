1fd4ee1be2df497cbe2cde8e3cf800f14776b0c6
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-10-02 19:09:22 +0300

    fonts directory add

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
-package go_invoice
+package main
 
 import (
+	"fmt"
 	"github.com/jung-kurt/gofpdf"
-	"log"
+	"os"
 )
 
+func main() {
+	New()
+}
+
 func New() {
-	pdf := gofpdf.New("P", "mm", "A4", "")
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	pdf := gofpdf.New("P", "mm", "A4", pwd+"/font/")
 	pdf.AddFont("Helvetica", "", "helvetica_1251.json")
 	pdf.AddPage()
 	pdf.SetFont("Helvetica", "", 16)
 	tr := pdf.UnicodeTranslatorFromDescriptor("cp1251")
-	pdf.Cell(15, 50, tr("русский текст"))
-	err := pdf.OutputFileAndClose("test.pdf")
-	if err != nil {
-		log.Println(err)
-	}
+	pdf.Cell(15, 50, tr("Слава Україні"))
+	pdf.OutputFileAndClose("test.pdf")
 }
